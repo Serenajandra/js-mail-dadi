cb62b5caab47f84ebd873e3d698bfb65e9da2048
Serenajandra <serena.jandra@gmail.com>
2022-10-11 16:54:48 +0200

    aggiunti btn per interagire con l'utente

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -4,19 +4,30 @@
 
 
 // generare numero computer
+
 const computerNumber = Math.floor(Math.random()*6)+1;
 console.log (computerNumber);
+const computerBtn = document.getElementById("computer_btn");
+
+computerBtn.addEventListener("click", function() {
+    alert(`il computer ha scelto il numero ${computerNumber}`)
+})
+
 // generare numero user
 const  userNumber = Math.floor(Math.random()*6)+1;
 console.log(userNumber);
 
-// SE numero computer > numero userNumber
-if(computerNumber > userNumber){
-//     allora vince computer
-    alert("Il computer ha vinto!");
-    // ALTRIMENTI vince user
-} else{
-    alert("ha vinto l'utente!");
-}
+const userBtn = document.getElementById("user_btn");
+
+userBtn.addEventListener("click", function(){
+    // SE numero computer > numero userNumber
+    if(computerNumber > userNumber){
+    //     allora vince computer
+        alert(`Purtroppo  hai estratto il numero ${userNumber} quindi il computer ha vinto`);
+        // ALTRIMENTI vince user
+    } else {
+        alert(`Hai estratto il numero ${userNumber}, quindi hai vinto!`);
+    }
 
 
+})
